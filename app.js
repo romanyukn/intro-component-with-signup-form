2b4ada75ec3c77391f64b3764c4d380397859ca6
Natalia Romanyuk <feliz@MacBook-Pro.modem>
2020-11-24 15:03:03 +1100

    fix validation

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const form = document.getElementsByTagName("input");
 const formArray = Array.from(form);
 const button = document.getElementById("button");
 
-function generate() {
+function validateLoginForm() {
   const formValue = [];
   formArray.map((el) => {
     if (el.value === "") {
@@ -18,5 +18,12 @@ function generate() {
 
 button.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log(generate());
+  console.log(validateLoginForm());
 });
+
+formArray.forEach((el) =>
+  el.addEventListener("change", (el) => {
+    console.log(el);
+    el.className = "form-control";
+  })
+);
