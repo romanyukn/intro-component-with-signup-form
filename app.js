6c073efbf43a743173510d2bbc0c2ac1824559e4
Natalia Romanyuk <feliz@MacBook-Pro.modem>
2020-11-27 14:24:30 +1100

    make error message dinamic

--- app.js
+++ app.js
@@ -24,15 +24,17 @@ formArray.forEach((el) =>
 function validateError(el) {
   if (el.value === "") {
     el.className = "form-control is-invalid";
+    el.parentNode.querySelector(".invalid-feedback").innerText = `${el.placeholder} cannot be empty`;
     return false
   };
   if(el.placeholder === "Email Address") return validateEmail(el);
   return true;
 }
 
-function validateEmail(email) {
-  if (!emailCheck.test(email.value)) {
-    email.className = "form-control is-invalid";
+function validateEmail(el) {
+  if (!emailCheck.test(el.value)) {
+    el.className = "form-control is-invalid";
+    el.parentNode.querySelector(".invalid-feedback").innerText = `Looks like this is not an ${el.placeholder}`;
     return false;
   }
   return true;
